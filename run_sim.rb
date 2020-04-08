1d6a40db41882c5dda8cb8ea0d9ca73d7201e36e
Hormoz K <hkheradm@gmail.com>
2020-04-08 11:00:03 -0400

    rescue interrupts for better output

--- run_sim.rb
+++ run_sim.rb
@@ -20,76 +20,84 @@ def draw_table(table, clear: true, notes: [])
   puts format(values, *table.map { |c| c[:value] })
 end
 
-# 700 upstreams per lb
-upstreams = []
-700.times do |i|
-  upstreams << Worker.new("upstream_#{i}", 20)
-end
-
-# 10 ms per tick
-jobs_per_tick = 7 # 700 jobs per second per lb
-job_dur = (20..80) # uniform: 200ms - 20000ms
-
-# 17 lbs per cluster
-lbs = []
-17.times do |i|
-  lbs << LB.new("lb_#{i}", upstreams, jobs_per_tick, job_dur)
-end
-
-samples = {
-  avg:                0,
-  std_dev:            0,
-  overloaded_count:   0,
-  overloaded_percent: 0,
-  count:              0,
-}
-
-duration_s = 30
-ticks = duration_s / 0.01
-ticks.to_i.times do |tick|
-  lbs.each { |l| l.tick }
-  upstreams.each { |u| u.tick }
-
-
-  utils = []
-  upstreams.each do |u|
-    utils << u.utilization
+def run_sim
+  # 700 upstreams per lb
+  upstreams = []
+  700.times do |i|
+    upstreams << Worker.new("upstream_#{i}", 16)
   end
 
-  avg = utils.sum / utils.size.to_f
-  sum_squared = utils.map { |u| (u-avg)**2 }.sum
-  variance = sum_squared / (utils.size - 1)
-  std_dev = Math.sqrt(variance)
+  # 10 ms per tick
+  jobs_per_tick = 7 # 700 jobs per second per lb
+  job_dur = (20..80) # uniform: 200ms - 20000ms
 
-  overloaded_thresholdd = avg + std_dev*3
-  overloaded_count = utils.count { |u| u > overloaded_thresholdd }
-  overloaded_percent = overloaded_count / utils.count.to_f
+  # 17 lbs per cluster
+  lbs = []
+  17.times do |i|
+    lbs << LB.new("lb_#{i}", upstreams, jobs_per_tick, job_dur)
+  end
 
-  samples[:avg] += avg.to_f
-  samples[:std_dev] += std_dev.to_f
-  samples[:overloaded_percent] += overloaded_percent.to_f
-  samples[:overloaded_count] += overloaded_count.to_f
-  samples[:count] += 1
+  samples = {
+    avg:                0,
+    std_dev:            0,
+    overloaded_count:   0,
+    overloaded_percent: 0,
+    count:              0,
+  }
 
-  time = tick.to_f/100
+  duration_s = 30
+  ticks = duration_s / 0.01
+  ticks.to_i.times do |tick|
+    lbs.each { |l| l.tick }
+    upstreams.each { |u| u.tick }
 
+
+    utils = []
+    upstreams.each do |u|
+      utils << u.utilization
+    end
+
+    avg = utils.sum / utils.size.to_f
+    sum_squared = utils.map { |u| (u-avg)**2 }.sum
+    variance = sum_squared / (utils.size - 1)
+    std_dev = Math.sqrt(variance)
+
+    overloaded_threshold = avg + std_dev*3
+    overloaded_count = utils.count { |u| u > overloaded_threshold }
+    overloaded_percent = overloaded_count / utils.count.to_f
+
+    samples[:avg] += avg.to_f
+    samples[:std_dev] += std_dev.to_f
+    samples[:overloaded_percent] += overloaded_percent.to_f
+    samples[:overloaded_count] += overloaded_count.to_f
+    samples[:count] += 1
+
+    time = tick.to_f/100
+
+    draw_table([
+      { title: "time"        , value: time               , width: 10 , type: :float } ,
+      { title: "avg"         , value: avg                , width: 10 , type: :float } ,
+      { title: "std_dev"     , value: std_dev            , width: 10 , type: :float } ,
+      { title: "#overloaded" , value: overloaded_count   , width: 15 , type: :int   } ,
+      { title: "%overloaded" , value: overloaded_percent , width: 15 , type: :float } ,
+    ], notes: [
+      "Note: overloaded is defined as workers with utilization above <avg + 3 standard deviations> (current_value=#{overloaded_threshold.round(2)})",
+      "",
+    ])
+  end
+
+  puts
+  puts "Total Averages"
   draw_table([
-    { title: "time"        , value: time               , width: 10 , type: :float } ,
-    { title: "avg"         , value: avg                , width: 10 , type: :float } ,
-    { title: "std_dev"     , value: std_dev            , width: 10 , type: :float } ,
-    { title: "#overloaded" , value: overloaded_count   , width: 15 , type: :int   } ,
-    { title: "%overloaded" , value: overloaded_percent , width: 15 , type: :float } ,
-  ], notes: [
-    "Overloaded = workers with utilization above <avg + 3 standard deviations>"
-  ])
+    { title: "avg"                , value: samples[:avg]/samples[:count]                , width: 20 , type: :float } ,
+    { title: "std_dev"            , value: samples[:std_dev]/samples[:count]            , width: 20 , type: :float } ,
+    { title: "overloaded_count"   , value: samples[:overloaded_count]/samples[:count]   , width: 20 , type: :float } ,
+    { title: "overloaded_percent" , value: samples[:overloaded_percent]/samples[:count] , width: 20 , type: :float } ,
+    { title: "count"              , value: samples[:count]/samples[:count]              , width: 20 , type: :float } ,
+  ], clear: false)
 end
 
-puts
-puts "Total Averages"
-draw_table([
-  { title: "avg"                , value: samples[:avg]/samples[:count]                , width: 20 , type: :float } ,
-  { title: "std_dev"            , value: samples[:std_dev]/samples[:count]            , width: 20 , type: :float } ,
-  { title: "overloaded_count"   , value: samples[:overloaded_count]/samples[:count]   , width: 20 , type: :float } ,
-  { title: "overloaded_percent" , value: samples[:overloaded_percent]/samples[:count] , width: 20 , type: :float } ,
-  { title: "count"              , value: samples[:count]/samples[:count]              , width: 20 , type: :float } ,
-], clear: false)
+begin
+  run_sim
+rescue Interrupt
+end
